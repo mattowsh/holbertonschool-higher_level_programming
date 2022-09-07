2868410f030af694bc5d5ed5ab10d41c9ed0b746
mattowsh <5089@holbertonschool.com>
2022-09-07 09:03:18 -0700

    task 2 header ifs were fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
 
 # to print the header
     print("{:d} argument".format(qty_args - 1), end="")
-    if (qty_args != 1):
+    if (qty_args != 2):
         print("s", end="")
-    if (qty_args > 0):
+    if (qty_args > 1):
         print(":")
-    if (qty_args == 0):
+    if (qty_args == 1):
         print(".")
 
 # to print all the args
