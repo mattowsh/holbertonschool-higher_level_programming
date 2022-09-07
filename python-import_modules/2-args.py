24dbc2a73bc76b76adb5774c6d75a3c2cafccb6d
mattowsh <5089@holbertonschool.com>
2022-09-07 08:59:36 -0700

    task 2 version 0

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    qty_args = len(sys.argv)
+
+# to print the header
+    print("{:d} argument".format(qty_args - 1), end="")
+    if (qty_args != 1):
+        print("s", end="")
+    if (qty_args > 0):
+        print(":")
+    if (qty_args == 0):
+        print(".")
+
+# to print all the args
+    for i in range(1, qty_args):
+        print("{:d}: {}".format(i, sys.argv[i]))
