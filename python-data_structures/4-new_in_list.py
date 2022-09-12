b2009547adc76d40d91ba9dcb7c359f268345a12
mattowsh <mattobelen@gmail.com>
2022-09-12 10:40:56 -0700

    task 4 version 0

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return
+
+    if (idx < 0) or (idx >= len(my_list)):
+        return my_list
+
+    list_copy = my_list.copy()
+
+    list_copy[idx] = element
+    return list_copy
