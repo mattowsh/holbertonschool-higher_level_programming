04aed45442e83cf6b8cf1840b116706378e61fe6
mattowsh <mattobelen@gmail.com>
2022-09-13 07:40:47 -0700

    task 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return
+
+    newlist = []
+    for i in my_list:
+        if i % 2 == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
+    return newlist
