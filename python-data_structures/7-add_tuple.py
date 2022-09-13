74e5859f04c1f7f4f95fa63910a0a9ab3be47a94
mattowsh <mattobelen@gmail.com>
2022-09-12 17:35:47 -0700

    case tuple_a and tuple_b was considered

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -12,6 +12,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif (len(tuple_b) == 0):
         return tuple_a[0], tuple_a[1]
 
+    elif (len(tuple_a) == 1) and (len(tuple_b) == 1):
+        return tuple_a[0], tuple_b[0]
+
     elif (len(tuple_a) == 1):
         return (tuple_a[0] + tuple_b[0]), tuple_b[1]
 
