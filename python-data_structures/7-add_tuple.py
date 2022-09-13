a77ff813635d627550036f66eb2a9556a6e3a43c
mattowsh <mattobelen@gmail.com>
2022-09-12 17:33:52 -0700

    returns were fixed

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if (len(tuple_a) >= 2) and (len(tuple_b) >= 2):
-        fst_sum = tuple_a[0] + tuple_b[0]
-        snd_sum = tuple_a[1] + tuple_b[1]
+        return (tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1])
 
     elif (len(tuple_a) == 0) and (len(tuple_b) == 0):
-        fst_sum = snd_sum = 0
+        return (0, 0)
 
     elif (len(tuple_a) == 0):
-        fst_sum = tuple_b[0]
-        snd_sum = tuple_b[1]
+        return tuple_b[0], tuple_b[1]
 
     elif (len(tuple_b) == 0):
-        fst_sum = tuple_a[0]
-        snd_sum = tuple_a[1]
+        return tuple_a[0], tuple_a[1]
 
-    elif (len(tuple_a) == 1) or (len(tuple_b) == 1):
-        fst_sum = tuple_a[0] + tuple_b[0]
-        snd_sum = 0
+    elif (len(tuple_a) == 1):
+        return (tuple_a[0] + tuple_b[0]), tuple_b[1]
 
-    print("({:d}, {:d})".format(fst_sum, snd_sum))
+    elif (len(tuple_b) == 1):
+        return (tuple_a[0] + tuple_b[0]), tuple_a[1]
