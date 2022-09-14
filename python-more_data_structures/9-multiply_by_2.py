2d71ad264b3d312fca5e5ee14488d2fe6ffdb993
mattowsh <mattobelen@gmail.com>
2022-09-14 13:38:00 -0700

    2nd version was contemplated

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -6,3 +6,7 @@ def multiply_by_2(a_dictionary):
     dict_copy = a_dictionary.copy()
     dict_copy.update((key, value*2) for key, value in a_dictionary.items())
     return dict_copy
+
+    # 2nd option:
+    # for key in dict_copy:
+    #   dict_copy[key] *= 2
