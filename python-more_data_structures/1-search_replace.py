a4eeddd30f94bc43a6f5ea8c6dac80d001132162
mattowsh <mattobelen@gmail.com>
2022-09-14 07:11:10 -0700

    task 1 version 0

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return
+
+    new_list = [replace if element == search else element for element in my_list]
+    return new_list
