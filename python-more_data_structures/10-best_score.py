1e21de9958f68be922e2b56a3b36d3f65d20946e
mattowsh <mattobelen@gmail.com>
2022-09-14 14:09:43 -0700

    task 10 version 0

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    max_value = 0
+    key_associated = ""
+    for key in a_dictionary:
+        if a_dictionary[key] > max_value:
+            max_value = a_dictionary[key]
+            key_associated = key
+
+    return key_associated
