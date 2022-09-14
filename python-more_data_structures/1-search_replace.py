e8a5463e19e1b5cbed60c7f87918407827d726bf
mattowsh <mattobelen@gmail.com>
2022-09-14 07:13:55 -0700

    pycodestyle fixed

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,5 +3,6 @@ def search_replace(my_list, search, replace):
     if my_list is None:
         return
 
-    new_list = [replace if element == search else element for element in my_list]
+# elem = element in my_list
+    new_list = [replace if elem == search else elem for elem in my_list]
     return new_list
