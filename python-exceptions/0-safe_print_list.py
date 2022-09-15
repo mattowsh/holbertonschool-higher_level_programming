058582bf6011e95e8773240d7673fcdd01f332cc
mattowsh <mattobelen@gmail.com>
2022-09-15 09:15:49 -0700

    task 0 version 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if my_list is None:
-        return 0
+    length = 0
+    length = sum(1 for item in my_list)
 
-    for i in my_list:
+    counter = 0
+    try:
+        for i in range(0, x):
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+    except IndexError:
         print()
+    else:
+        print()
+    finally:
+        return counter
