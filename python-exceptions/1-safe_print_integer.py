fc0248a0a8f07cac1cd678b24062c5e2f11e48b8
mattowsh <mattobelen@gmail.com>
2022-09-15 08:29:55 -0700

    task 1 with notes

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,3 +6,7 @@ def safe_print_integer(value):
     except:
         return value_type
     return value_type
+
+    # if the type of value == 2nd parameter in isinstance,
+    # this function return True
+    # I will entry in except block if value_type is False
