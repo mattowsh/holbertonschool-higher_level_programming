a6b2e38c62dcbc948136eef380344970dec81d3e
mattowsh <mattobelen@gmail.com>
2022-09-19 11:30:12 -0700

    task 3 version0

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Class that defines a square with attributes"""
+    def __init__(self, size=0):
+        self.__size = size
+
+        if isinstance(size, int) == 0:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Calculates the area of a square"""
+        return self.__size * self.__size
