4124a2bd236b4419893ceec772e7a2ed3b3e5388
mattowsh <mattobelen@gmail.com>
2022-09-19 07:57:18 -0700

    module fixed version 1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Square class"""
 class Square:
+
     """Empty class that defines a square"""
     # this class doesn't has any attribute, so I created an empty block:
     pass
