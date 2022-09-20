48fb03c3595de0ff5793ebcc0fc3a7f67f7ad860
mattowsh <mattobelen@gmail.com>
2022-09-20 06:57:57 -0700

    square rows was fixed

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -30,4 +30,6 @@ class Square:
             for i in range(0, self.__size):
                 for j in range(0, self.__size):
                     print("#", end="")
-        print()
+                print()
+        else:
+            print()
