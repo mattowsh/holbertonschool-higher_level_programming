f8f79897d466bccfcf1252c7f7ddf5775f57089f
mattowsh <mattobelen@gmail.com>
2022-09-20 06:47:54 -0700

    task 5 version 0

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Class that defines a square with attributes"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Determinate if the value is or not a int"""
+        if isinstance(value, int) == 0:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of a square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square with the # character"""
+        for i in range(0, self.__size):
+            if self.__size == 0:
+                print()
+            else:
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
