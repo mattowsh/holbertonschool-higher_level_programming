03acd2e59d0fdcd9e774475598aabf169d43e8de
mattowsh <mattobelen@gmail.com>
2022-09-20 11:49:32 -0700

    if positions were fixed and moved in position setter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -32,9 +32,9 @@ class Square:
         """To setter position attribute"""
         if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
