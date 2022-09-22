f512dcd0c8f26e7afeed5dab3e09970d22e28e1e
mattowsh <mattobelen@gmail.com>
2022-09-21 18:48:08 -0700

    task 5 version 0

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Task 5 module """
+
+
+def text_indentation(text):
+    """ Prints a text with 2 new lines after each
+        of these characters: ., ? and : """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    find_character = 0
+    for i in range(len(text)):
+        if text[i] is "." or text[i] is "?" or text[i] is ":":
+            print(text[i] + "\n")
+            find_character = 1
+        elif find_character == 1:
+            find_character = 0
+            continue
+        else:
+            print(text[i], end="")
