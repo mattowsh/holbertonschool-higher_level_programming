9aa2b10cdfe0050f2484bcb011f18e490742aadc
mattowsh <mattobelen@gmail.com>
2022-09-25 13:35:19 -0700

    fixed position of counter

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -2,15 +2,15 @@
 """Rectangle class"""
 
 
-number_of_instances = 0
-
 class Rectangle:
-    """Class that defines a rectangle with attributes"""
+    """Class that defines a rectangle to create a new instance"""
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
-        return
 
     @property
     def width(self):
