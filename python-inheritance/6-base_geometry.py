55b89acceb87c2eb5c8fe6f6b9ffe8435dcf1777
mattowsh <mattobelen@gmail.com>
2022-09-26 10:57:08 -0700

    task 6 version 0

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    pass
+
+    def area(self):
+        """Calculates the area"""
+        raise Exception("area() is not implemented")
