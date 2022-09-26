ccaaf766a208dbef604927ed21194be1a9f3ca64
mattowsh <mattobelen@gmail.com>
2022-09-26 07:43:14 -0700

    task 1 version 0

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class MyList that inherits from list"""
+
+
+class MyList(list):
+    """Generates a new class that inherits attributes and
+    methods of the baseclass list"""
+    
+    def print_sorted(self):
+        """Prints the sorted list: ascending order"""
+        sorted_list = self.copy()
+        sorted_list.sort()
+        print(sorted_list)
