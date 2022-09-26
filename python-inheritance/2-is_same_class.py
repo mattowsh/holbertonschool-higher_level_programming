84aeff400eade9a5eb25bef2df1d8ec15f750eaf
mattowsh <mattobelen@gmail.com>
2022-09-26 10:01:36 -0700

    task 1 return was fixed

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""..."""
-
+"""is_same_class function"""
 
 def is_same_class(obj, a_class):
     """Returns True if the object is exactly an instance
     of the specified class ; otherwise False"""
 
-    def is_same_class(obj, a_class):
-        """Check the type of an instance"""
-        return type(obj) is a_class
+    return type(obj) is a_class
