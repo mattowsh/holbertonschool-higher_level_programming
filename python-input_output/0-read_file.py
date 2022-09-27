91d34d26cebb368ded9c68e21eff1159919a54e1
mattowsh <mattobelen@gmail.com>
2022-09-27 07:26:10 -0700

    module was documentated

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Task 0 module: reads a text file and prints it using with statement
+"""
+
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
