2b1296d2626bfa327d32d0fa333aa9b66df5dc5e
mattowsh <mattobelen@gmail.com>
2022-09-27 08:17:49 -0700

    task 3 version 0

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Task 3 module: To JSON string
+"""
+import json
+
+def to_json_string(my_obj):
+    """Return: the JSON representation of an object (string)"""
+    if my_obj is None:
+        return
+    else:
+        return json.dumps(my_obj)
