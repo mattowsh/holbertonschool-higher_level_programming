cd6a6e76702db16c7c445aca4db36ae14a12552c
mattowsh <mattobelen@gmail.com>
2022-09-27 07:28:40 -0700

    final newline was deleted

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ Task 0 module: reads a text file and prints it using with statement
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
