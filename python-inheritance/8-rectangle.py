df7ec92091b3dbf3d111ac4b391b1f6bcfdca751
mattowsh <mattobelen@gmail.com>
2022-09-26 17:26:10 -0700

    readme gral update for new project; reorganize folders; issubclass task 8 was fixed

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,24 +1,6 @@
 #!/usr/bin/python3
-"""BaseGeometry class"""
-
-
-class BaseGeometry:
-    """Class that define a BaseGeometry object"""
-    pass
-
-    def area(self):
-        """Calculates the area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates value"""
-        # we can assume @name is always a string
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
 """Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
