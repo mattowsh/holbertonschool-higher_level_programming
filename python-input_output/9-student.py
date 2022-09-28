47e41d16f2862a49b595ffa80090eda2fdad8cc4
mattowsh <mattobelen@gmail.com>
2022-09-28 09:41:24 -0700

    task 9 verion 0

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -2,7 +2,17 @@
 """
 Task 9 module
 """
-import json
 
+class Student():
+    """Class Student with attributes"""
 
+    def __init__(self, first_name, last_name, age):
+        """To create a new instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
 
