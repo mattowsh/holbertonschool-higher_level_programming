b7c42e8876d57245caefd6961990b9adc0e1e084
mattowsh <mattobelen@gmail.com>
2022-09-28 11:31:08 -0700

    task 10 version 0

---
+++ python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Task 10 module
+"""
+
+
+class Student():
+    """Class Student with attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        """To create a new instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if type(attrs) == list:
+            new_dict = {}
+            for item in attrs:
+                for key, value in self.__dict__.items():
+                    if key == item:
+                        new_dict[item] = value
+            return new_dict
+                    
+        return self.__dict__
