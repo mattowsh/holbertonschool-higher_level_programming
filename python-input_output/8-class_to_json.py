54f6b3e2d35093a99aef44855c2da3b3cceda1bd
mattowsh <mattobelen@gmail.com>
2022-09-28 09:33:44 -0700

    task 8 version 0

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Task 8 module: Class to JSON
+"""
+import json
+
+
+def class_to_json(obj):
+    """R the dictionary description with simple data structure for
+    JSON serialization of an object"""
+    return obj.__dict__
