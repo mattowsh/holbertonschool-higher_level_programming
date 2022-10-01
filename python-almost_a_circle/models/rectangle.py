82e984da5d9968343cf39d02ceb99ef8ab0c4aa5
mattowsh <mattobelen@gmail.com>
2022-10-01 12:41:26 -0700

    task 3 version 0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -24,6 +24,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ To set the private width attribute: setter """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -34,6 +38,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ To set the private height attribute: getter """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -44,6 +52,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ To set the private x attribute: getter """
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -54,4 +66,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ To set the private y attribute: getter """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
