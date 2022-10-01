5f1c0b02615fb92ae297dedc3546fc6b1f1769a6
mattowsh <mattobelen@gmail.com>
2022-10-01 13:09:58 -0700

    task 5 version 0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,3 +75,10 @@ class Rectangle(Base):
     def area(self):
         """ Calculates the area of a rectangle """
         return self.__width * self.__height
+
+    def display(self):
+        """ Prints in stdout the Rectangle instance with the character # """
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
