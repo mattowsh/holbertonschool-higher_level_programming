e3968b474ee48b2bd37a4ceda089f8dc066f7aea
mattowsh <mattobelen@gmail.com>
2022-10-05 11:17:04 -0700

    rectangle tests first version, arguments were tested

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,7 +3,37 @@
 Unittest for class Rectangle methods
 """
 import unittest
-import json
-from models.base import Base
 from models.rectangle import Rectangle
 
+
+class Test_classRectangle(unittest.TestCase):
+
+    def test_multiple_parameters(self):
+        """ check the new instance with 2 arguments """
+        rect0 = Rectangle(1, 2)
+        self.assertEqual(rect0.width, 1)
+        self.assertEqual(rect0.height, 2)
+
+        rect1 = Rectangle(1, 2, 3)
+        self.assertEqual(rect1.width, 1)
+        self.assertEqual(rect1.height, 2)
+        self.assertEqual(rect1.x, 3)
+
+        rect2 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(rect2.width, 1)
+        self.assertEqual(rect2.height, 2)
+        self.assertEqual(rect2.x, 3)
+        self.assertEqual(rect2.y, 4)
+
+    def test_width_nonint(self):
+        """ check if the arguments are not int values """
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+            Rectangle(1, "2")
+            Rectangle(1, 2, "3")
+            Rectangle(1, 2, 3, "4")
+
+    def test_id_ok(self):
+        """ check to check id of the class, passed as argument """
+        rect = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(rect.id, 5)
