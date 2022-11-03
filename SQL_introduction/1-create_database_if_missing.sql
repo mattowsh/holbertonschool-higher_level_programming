df9388381e750dac85ca4c23e845b37561098794
mattowsh <mattobelen@gmail.com>
2022-11-03 20:37:25 -0300

    task 1 syntax was fixed: create command

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -2,4 +2,4 @@
 -- creates the database hbtn_0c_0 in your MySQL server
 -- if the database already exists, your script should not fail
 
-CREATES DATABASE IF NOT EXISTS hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
