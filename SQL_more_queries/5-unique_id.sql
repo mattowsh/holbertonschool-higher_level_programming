06532a436195c6bae1a1ae367cf1b9599b854285
mattowsh <mattobelen@gmail.com>
2022-11-04 20:02:10 -0300

    task 5: DEFAULT FEATURE was added

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -2,5 +2,5 @@
 -- creates the table unique_id on your MySQL server
 
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT NOT NULL UNIQUE,
+    id INT NOT NULL UNIQUE DEFAULT 1,
     name VARCHAR(256));
