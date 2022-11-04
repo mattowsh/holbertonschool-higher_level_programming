e58b227a0ade9093cdc2e195d09e5d38ff7ad120
mattowsh <mattobelen@gmail.com>
2022-11-04 20:21:54 -0300

    task 6: path table was fixed

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -6,6 +6,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Creates the table in the created database
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL);
