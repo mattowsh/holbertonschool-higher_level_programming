dfa54a5b7f347216642e84c52605d38353a24c73
mattowsh <mattobelen@gmail.com>
2022-11-04 19:39:53 -0300

    task 2 version 0

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,15 @@
+-- Task 2:
+-- creates the database hbtn_0d_2 and the user user_0d_2
+
+-- Creates the database:
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Creates the user:
+CREATE USER
+IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Privileges setting
+GRANT SELECT 
+    ON hbtn_0d_2.*
+    TO 'user_0d_2'@'localhost';
