d2ded7abd5b1d049c33ded2d343e4bbb33cc831e
mattowsh <mattobelen@gmail.com>
2022-11-04 23:32:45 -0300

    task 9: from states declaration fixed

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,7 +2,7 @@
 -- lists all cities contained in the database hbtn_0d_usa
 
 SELECT DISTINCT c.id, c.name, s.name
-FROM cities c, sites s
+FROM cities c, states s
 WHERE
     c.state_id = s.id
 ORDER BY c.id ASC;
