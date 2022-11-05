b96d320d335f18969bfad017b9db1f658e5fdd52
mattowsh <mattobelen@gmail.com>
2022-11-05 00:35:07 -0300

    task 12 version 0

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,12 @@
+-- Task 12:
+-- 1. import the database dump from hbtn_0d_tvshows to your MySQL server
+-- 2. lists all shows contained in hbtn_0d_tvshows without a genre linked
+
+SELECT sh.title, genres.genre_id
+FROM tv_shows sh
+-- We want the records with NULL genre_id:
+LEFT JOIN tv_show_genres genres
+ON sh.id = genres.genre_id
+WHERE
+    genres.genre_id IS NULL
+ORDER BY sh.title, genres.genre_id ASC;
