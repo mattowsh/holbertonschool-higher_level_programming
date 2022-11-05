82887763a170baf391cfd28bfdb38a17244f6023
mattowsh <mattobelen@gmail.com>
2022-11-05 01:11:54 -0300

    task 13: alias were changed to keep a clean code

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,10 +3,10 @@
 -- 2. lists all genres from hbtn_0d_tvshows and displays the number of
 -- shows linked to each
 
-SELECT genres.name, COUNT(sh_genres.genre_id)
-FROM tv_genres genres
+SELECT ge.name, COUNT(sh_ge.genre_id)
+FROM tv_genres ge
 
-INNER JOIN tv_show_genres sh_genres
-ON genres.id = sh_genres.genre_id
+INNER JOIN tv_show_genres sh_ge
+ON ge.id = sh_ge.genre_id
 
-ORDER BY COUNT(sh_genres.genre_id) DESC;
+ORDER BY COUNT(sh_ge.genre_id) DESC;
