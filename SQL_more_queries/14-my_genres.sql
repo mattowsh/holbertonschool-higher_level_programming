842171ff309916b169515bb936b154cdf67a0d30
mattowsh <mattobelen@gmail.com>
2022-11-05 14:23:13 -0300

    task 14: sql keyword AS was modified: uppercase

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 -- 1. import the database dump from hbtn_0d_tvshows to your MySQL server
 -- 2. uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 
-SELECT ge.name as name
+SELECT ge.name AS name
 FROM 
     tv_genres ge INNER JOIN tv_show_genres sh_ge
 ON 
