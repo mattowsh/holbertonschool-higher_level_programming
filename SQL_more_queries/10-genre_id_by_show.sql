4b7cd48f97e5f67fcad965d5130bc1414e4f4eee
mattowsh <mattobelen@gmail.com>
2022-11-05 00:00:02 -0300

    task 11 version 0

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,10 @@
 -- 2. lists all shows contained in hbtn_0d_tvshows that have at least
 -- one genre linked
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT sh.title, genres.genre_id
+FROM tv_shows sh
+-- We want compare two columns of different tables, and return the intersection
+-- thus, we want return only the records when these values are the same:
+INNER JOIN tv_show_genres genres
+ON sh.id = genres.show_id
+ORDER BY sh.title, genres.genre_id ASC;
