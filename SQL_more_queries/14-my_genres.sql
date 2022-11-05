3c6fdae29c5fc715d82a78a09cb2c1a58c63105d
mattowsh <mattobelen@gmail.com>
2022-11-05 14:21:56 -0300

    task 14 version 0

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,11 @@
+-- Task 14:
+-- 1. import the database dump from hbtn_0d_tvshows to your MySQL server
+-- 2. uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+
+SELECT ge.name as name
+FROM 
+    tv_genres ge INNER JOIN tv_show_genres sh_ge
+ON 
+    ge.id = sh_ge.genre_id
+WHERE
+    sh_ge.show_id = 8 -- according to tv_shows table: show id 8 = Dexter
