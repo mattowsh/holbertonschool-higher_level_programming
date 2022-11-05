757dacb90980dac48ba33808e4fc9a7c9b799696
mattowsh <mattobelen@gmail.com>
2022-11-04 23:27:53 -0300

    task 9: select DISTINCT was added to avoid duplicated cities

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- Task 9:
 -- lists all cities contained in the database hbtn_0d_usa
 
-SELECT c.id, c.name, s.name
+SELECT DISTINCT c.id, c.name, s.name
 FROM cities c, sites s
 WHERE
     c.state_id = s.id
