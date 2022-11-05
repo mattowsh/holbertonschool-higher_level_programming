3b8f7c335714536cc7414e3c0b23df5b265db11a
mattowsh <mattobelen@gmail.com>
2022-11-05 15:34:17 -0300

    task 16 version 0

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,14 @@
+-- Task 16:
+-- 1. import the database dump from hbtn_0d_tvshows to your MySQL server
+-- 2. lists all shows, and all genres linked to that show, from the
+-- database hbtn_0d_tvshows
+
+SELECT sh.title, ge.name
+FROM
+    tv_shows sh INNER JOIN tv_show_genres sh_ge
+ON
+    sh.id = sh_ge.show_id INNER JOIN tv_genres ge
+ON
+    ge.id = sh_ge.genre_id
+ORDER BY
+    sh.title, ge.name
