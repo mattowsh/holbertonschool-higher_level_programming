5a0ded58d4645745335cb8665d8766aadfc4c5ad
mattowsh <mattobelen@gmail.com>
2022-11-05 00:21:14 -0300

    task 11 version 0

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,12 @@
+-- Task 11:
+-- 1. import the database dump from hbtn_0d_tvshows to your MySQL server
+-- 2. lists all shows contained in the database hbtn_0d_tvshows
+
+SELECT sh.title, genre.genre_id
+FROM tv_shows.title sh
+-- In this case we want return all elements of the A table (sh), no matter
+-- if has or not a value in the B table (genre):
+LEFT JOIN tv_show_genres.genre_id genre
+ON
+    sh.id = genre.show_id
+ORDER BY sh.title, genre.genre_id ASC;
