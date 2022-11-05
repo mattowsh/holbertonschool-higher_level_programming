c0e0b806a43059f3a6e0ad8e6d35720536818221
mattowsh <mattobelen@gmail.com>
2022-11-05 00:26:11 -0300

    task 11: tables aliases were fixed

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,10 +3,10 @@
 -- 2. lists all shows contained in the database hbtn_0d_tvshows
 
 SELECT sh.title, genre.genre_id
-FROM tv_shows.title sh
+FROM tv_shows sh
 -- In this case we want return all elements of the A table (sh), no matter
 -- if has or not a value in the B table (genre):
-LEFT JOIN tv_show_genres.genre_id genre
+LEFT JOIN tv_show_genres genre
 ON
     sh.id = genre.show_id
 ORDER BY sh.title, genre.genre_id ASC;
