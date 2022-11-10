ffd67c10fd5287b41a4abeb60ad4a98c14f22aeb
mattowsh <mattobelen@gmail.com>
2022-11-10 20:36:41 -0300

    task 0 version 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Task 0: lists all states from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_pwd = sys.argv[2]
+    mysql_dbname = sys.argv[3]
+
+    # to connect my target db: establish the connector:
+    my_db = MySQLdb.connect(
+        host="localhost",
+        port="3306",
+        user=mysql_username,
+        pwd=mysql_pwd,
+        # db I want to connect to: 
+        db=mysql_dbname)
+
+    # to execute the query of interest --> my SQL request:
+    qry_cursor = my_db.cursor()
+
+    sql_request = "SELECT * FROM states ORDER BY id ASC"
+    qry_cursor.execute(sql_request)
+    
+    # fetchall statement selects all data from the state table:
+    records = sql_request.fetchall()
+
+    # print all records:
+    for element in records:
+        print(element)
+
+    my_db.close()
