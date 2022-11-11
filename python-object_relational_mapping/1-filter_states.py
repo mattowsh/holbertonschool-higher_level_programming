7924d00a92fa90bf2cbf79dec6fa58bf69f360c7
mattowsh <mattobelen@gmail.com>
2022-11-11 20:56:34 -0300

    task 1. new try to detect documentation

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Task 1: lists all states with a name starting with N from the database
-hbtn_0e_0_usa
+""" Task 1: lists all states with a name starting with N from the database hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
@@ -15,8 +13,8 @@ if __name__ == "__main__":
     my_db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username
-        password=mysql_pwd
+        user=mysql_username,
+        password=mysql_pwd,
         db=mysql_dbname)
 
     qry_cursor = my_db.cursor()
