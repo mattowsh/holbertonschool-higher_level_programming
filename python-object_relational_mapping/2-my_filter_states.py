439123a3924cd1a3acb5021353b955a68c491aef
mattowsh <mattobelen@gmail.com>
2022-11-10 22:25:22 -0300

    task 2. sql_request was fixed: where condition and '{}'

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,8 +21,7 @@ if __name__ == "__main__":
         db=mysql_dbname)
 
     qry_cursor = my_db.cursor()
-    sql_request = "SELECT * FROM states WHERE state LIKE {} ORDER BY id ASC".format
-    (mysql_tomatch)
+    sql_request = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(mysql_tomatch)
     qry_cursor.execute(sql_request)
     records = qry_cursor.fetchall()
 
