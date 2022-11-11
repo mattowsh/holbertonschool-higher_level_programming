e51f66b6e437ebd9dbc464f47e2205538238bd39
mattowsh <mattobelen@gmail.com>
2022-11-10 22:35:39 -0300

    task 2. pep8 trailing whitespace ok / task 1. sql_request line long was fixed

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
         db=mysql_dbname)
 
     qry_cursor = my_db.cursor()
-    sql_request = "SELEC * FROM states WHERE state LIKE BINARY 'N%' ORDER BY id ASC"
+    sql_request = """SELEC * FROM states WHERE state LIKE BINARY 'N%' ORDER BY
+        id ASC"""
     qry_cursor.execute(sql_request)
     records = qry_cursor.fetchall()
 
