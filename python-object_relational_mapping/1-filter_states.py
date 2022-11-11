12574f6f94b852b0997efb5a097c7c0259e053a3
mattowsh <mattobelen@gmail.com>
2022-11-10 21:13:24 -0300

    task 1. documentation doesn't be reconognized: test all between  without spaces

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" Task 1: lists all states with a name starting with N from the
-database hbtn_0e_0_usa
-"""
+""" Task 1: lists all states with a name starting with N from the database hbtn_0e_0_usa """
 
 if __name__ == "__main__":
     import sys
