024a4749e30ac82b0601eacaa99971153e56b6d6
mattowsh <mattobelen@gmail.com>
2022-11-10 21:19:23 -0300

    task 1. documentation doesn't be reconognized: tab line 3

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Task 1: lists all states with a name starting with N from the database hbtn_0e_0_usa
+    Task 1: lists all states with a name starting with N from the database
+    hbtn_0e_0_usa
 """
-
 if __name__ == "__main__":
     import sys
     import MySQLdb
