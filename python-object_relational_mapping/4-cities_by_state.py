b2c29b6aefcbcd18c7899d94465f066fea8ad994
mattowsh <mattobelen@gmail.com>
2022-11-11 09:32:29 -0300

    task 4 version 0

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Task 4: lists all cities from the database hbtn_0e_4_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_pwd = sys.argv[2]
+    mysql_dbname = sys.argv[3]
+
+    my_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        password=mysql_pwd,
+        db=mysql_dbname)
+
+    qry_cursor = my_db.cursor()
+    qry_cursor.execute("""SELECT *
+                        FROM cities
+                        ORDER BY id ASC""")
+    records = qry_cursor.fetchall()
+
+    for element in records:
+        print(element)
+
+    my_db.close()
