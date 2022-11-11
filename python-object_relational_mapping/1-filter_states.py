03190f91b12fe831e82ae51a920e7bde4d9640f7
mattowsh <mattobelen@gmail.com>
2022-11-10 21:01:43 -0300

    task 1 version 0

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Task 1: lists all states with a name starting with N from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_pwd = sys.argv[2]
+    mysql_dbname = sys.argv[3]
+
+    my_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username
+        password=mysql_pwd
+        db=mysql_dbname)
+
+    qry_cursor = my_db.cursor()
+    sql_request = "SELEC * FROM states WHERE state LIKE BINARY 'N%'"
+    qry_cursor.execute(sql_request)
+    records = qry_cursor.fetchall()
+
+    for element in records:
+        print(element)
+
+    my_db.close()
