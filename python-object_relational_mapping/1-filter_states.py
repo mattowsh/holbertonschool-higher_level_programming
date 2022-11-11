ea20e5470e52529fc0f1690df0dcfdef86e6feb0
mattowsh <mattobelen@gmail.com>
2022-11-10 21:09:04 -0300

    task 1. first space was deleted

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Task 1: lists all states with a name starting with N from the
+""" Task 1: lists all states with a name starting with N from the
 database hbtn_0e_0_usa
 """
 
