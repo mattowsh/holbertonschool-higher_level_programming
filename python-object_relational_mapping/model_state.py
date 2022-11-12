f7b8394c5e347eda384ace29407a2597ce261a1c
mattowsh <mattobelen@gmail.com>
2022-11-11 22:33:40 -0300

    task 6. if __name__ was deleted

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,20 +4,19 @@ Task 6: python file that contains the class definition of a State and
 an instance Base = declarative_base()
 """
 
-if __name__ == "__main__":
-    import sys
-    from sqlalchemy import Column, Integer, String
-    from sqlalchemy.ext.declarative import declarative_base
+import sys
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-    Base = declarative_base()
+Base = declarative_base()
 
     
-    class State(Base):
-        """ New class to generate a table """
+class State(Base):
+    """ New class to generate a table """
 
 
-        __tablename__ = "states"
-        # Defines new class attributes:
-        id = Column(Integer, autoincrement=True, unique=True,
-                    primary_key=True, nullable=False)
-        name = Column(String(128), nullable=False)
+    __tablename__ = "states"
+    # Defines new class attributes:
+    id = Column(Integer, autoincrement=True, unique=True,
+                primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
