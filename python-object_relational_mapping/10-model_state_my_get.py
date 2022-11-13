46584beafe51f2c6d4a4d7491f01966d753bff1f
mattowsh <mattobelen@gmail.com>
2022-11-13 18:17:44 -0300

    task 11 version 0

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -34,3 +34,4 @@ if __name__ == "__main__":
         print("Not found")
 
     session.close()
+    
---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Task 11: adds the State object “Louisiana” to the database hbtn_0e_6_us.
+You must use the module SQLAlchemy
+"""
+
+import sys
+import MySQLdb
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_dbname = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+                            mysql_username, mysql_password, mysql_dbname))
+    Base.metadata.create_all(bind=engine)
+
+    # Create the new object = new record:
+    state_LA = State(id = 6, name = "Louisiana")
+
+    # Create the new session like always:
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Adding objects to the session doesn't actually writes them to the
+    # database, it only prepares the objects to be saved in the next commit:
+    session.add(state_LA)
+
+    # Now yes, we will finally add the new object.
+    # add_all() method accepts a list of objects to be added to the session:
+    session.add_all([state_LA])
+
+    # Save the objects to the database call commit() method:
+    session.commit()
+
+    # Makes the request and print the records :)
+    results = session.query(State).all()
+
+    for element in results:
+        print("{}: {}".format(element.id, element.name))
+
+    session.close()
