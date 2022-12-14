81736c1180074629a50f727409008e1e126733dc
mattowsh <mattobelen@gmail.com>
2022-12-14 11:44:30 -0300

    task 3 version 0

---
+++ javascript-DOM-manipulation/3-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Toggles the class of the header element when the user clicks on the
+// tag id toggle_header
+
+const button = document.querySelector('#toggle_header');
+button.addEventListener('click', () => toggleClass());
+
+function toggleClass () {
+  const element = document.querySelector('header');
+  element.classList.toggle('red');
+  element.classList.toggle('green');
+}
