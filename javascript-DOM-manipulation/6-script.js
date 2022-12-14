1aac1e82b9233b208f91bf800067b187223edba7
mattowsh <mattobelen@gmail.com>
2022-12-14 15:32:26 -0300

    task 6 version 0

--- javascript-DOM-manipulation/6-script.js
+++ javascript-DOM-manipulation/6-script.js
@@ -2,3 +2,11 @@
 // Fetches the character name from this URL:
 // https://swapi-api.hbtn.io/api/people/5/?format=json
 // The name must be displayed in the HTML tag with id 'character'
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const element = document.querySelector('#character');
+    element.innerHTML = `<ul><li>${data.name}</li></ul>`;
+  })
+  .catch(error => console.log(error));
